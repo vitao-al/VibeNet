fa86839759edb21eeae735416800e46f0c472e55
Victor Manuel <vm74531@gmail.com>
2026-03-15 04:37:34 -0300

    feat[index.html]: Add buttom edit profile

--- src/controller/teste.js
+++
@@ -1,8 +0,0 @@
-
-export async function get_users(){
-   const users = fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-  return console.log(users);
-}
